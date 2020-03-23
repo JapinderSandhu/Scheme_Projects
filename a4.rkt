d1d584d079ed0a2a888f585cd84189fa83241b26
JapinderSandhu <salutations@japi.ca>
2020-03-22 21:52:29 -0400

    Completed with tests :sparkles:

--- a4.rkt
+++ a4.rkt
@@ -4,22 +4,17 @@
 
 "Question 4."
 
-;append 
-(define (append L1 L2)
-  (cond ((null? L1) L2)
-        (else (cons (car L1)
-               (append (cdr L1) L2)))))
-
 ;exists-node
-(define (exists-node x list)
-  
+(define (exists-node x lst)
+
+
     (cond
       
-        ((null? list) #f)
+        ((null? lst) #f)
         ;check if the first element of the first element = x 
-        ( (eq? (caar list) x) #t)
+        ( (eq? (caar lst) x) #t)
         ;else tail recursion
-        (else (exists-node x (cdr list)))
+        (else (exists-node x (cdr lst)))
 
         )
 
@@ -43,24 +38,95 @@
 ;exists-edge
 (define (exists-edge x y list)
 
-
-  (newline)
-  (display list)
   
-  (cond ( (not (and (exists-node x list) (exists-node y list)) ) #f )
+  (cond ( (not (exists-node x list)  ) #f )
 
         ;base case
         ((null? list) #f)
-
+        
         ;check if the first element of the first element = x
         ;and if y is in that list
-        ( (and (eq? (caar list) x) (exists-element y  (car list) ) ) #t)
+        ( (and (eq? (caar list) x) (exists-element y  (cdr(car list)) ) ) #t)
 
         ;else tail recursion
-        (else (exists-edge x (cdr list)))
+        (else (exists-edge x y (cdr list)))
 
         )
-  
+
+  )
+
+;add-edge-recursive
+
+
+(define (add-edge-recursive x y lst)
+
+ 
+  (cond
+      
+    ( (null? lst) #f)
+        
+        ;check if the first element of the first element = x 
+        ( (eq? (caar lst) x) (cons (append (list (caar lst) y) (cdr (car lst) )) (cdr lst)) )
+        
+        ;else tail recursion
+        (else (add-edge-recursive x y (append (cdr lst) (cons (car lst) '() )) ) )
+
+        )
+
+  )
+;remove-edge-recursive
+(define (remove-edge-recursive x y lst)
+
+
+  (cond
+      
+    ( (null? lst) #f)
+        
+        ;check if the first element of the first element = x
+        ;search edge list of node, 
+        ( (eq? (caar lst) x) (cons (remove-element y (car lst) ) (cdr lst)  ) )
+        
+        ;else tail recursion
+        (else (remove-edge-recursive x y (append (cdr lst) (cons (car lst) '() )) ) )
+
+        )
+
+  )
+;remove-element
+
+(define (remove-element x lst)
+ 
+    (cond
+      
+        ((null? lst) #f)
+        
+        ;check if the first element of the first element = x  
+        ( (eq? (car lst) x)  (reverse (cdr lst)) )
+        
+        ;else tail recursion
+        (else (remove-element x  (append (cdr lst) (cons (car lst) '() )) ) )
+
+        )
+
+  )
+
+
+
+;remove-node-helper
+
+(define (remove-node-helper x lst)
+
+    (cond
+      
+        ((null? lst) #f)
+        
+        ;check if the first element of the first element = x  
+        ( (eq? (caar lst) x)  (cdr lst) )
+        
+        ;else tail recursion
+        (else (remove-node-helper x  (append (cdr lst) (cons (car lst) '() )) ) )
+
+        )
 
   )
 
@@ -68,6 +134,8 @@
 (define (make-graph)
 
   (define globalList '() )
+
+
 
    (define (add-node x)
       
@@ -84,33 +152,88 @@
   (define (add-edge x y)
 
      
-     ;if edge exist, return false
-     (cond ( (exists-edge x y globalList) #f)
+    
+    (cond
 
-
-           ;if nodes do not exist, return false
-           ( (not (and (exists-node x globalList) (exists-node y globalList) )) #f)
-
+      ;if node does not exist, return false
+      ( (not (and (exists-node x globalList) (exists-node y globalList) )) #f)
            
-           ;create edge if it doesnt exist
-           (else (begin
+      ;if edge exist, return false
+      ( (exists-edge x y globalList)  #f) 
+
+      ;create edge if it doesnt exist
+      (else (begin
               
-             (set! globalList (cons x globalList) )
-                
-             #t) ) )
+              (set! globalList (add-edge-recursive x y globalList) )
+           
+              #t)) 
+        
+      )
     )
 
-   (define (remove-node x y)
-      
-        (set! balance (+ balance amount))
-      
-        balance)
+    (define (remove-node x)
+
+      ;if node exists, remove
+        (if (exists-node x globalList)
+            
+            (begin 
+              ;find node and remove 
+                (set! globalList (remove-node-helper x globalList) )
+                
+                #t)
+            
+           #f) )
 
    (define (remove-edge x y)
       
-        (set! balance (+ balance amount))
+     (cond
+
+       ;if node does not exist, return false
+       ( (not (exists-node x globalList)) #f)
+           
+       ;if edge does not exist, return false
+       ( (not (exists-edge x y globalList) )  #f)
+
+       ;create edge if it doesnt exist
+       (else (begin
+              
+               (set! globalList (remove-edge-recursive x y globalList) )
+           
+               #t)) 
       
-        balance)
+        )
+     )
+
+  (define (display-list-helper lst)
+    
+    (newline)
+      (display (caar lst))
+      (display ": ")
+      (display (cdr (car lst) ) )
+      (newline)
+
+      (cond ( (null? (cdr lst) ) (display " ") )
+
+           
+            (else (display-list-helper (cdr lst) ) )
+
+
+            )
+
+
+    )
+
+    (define (display-list)
+      
+      (newline)
+      (display globalList)
+      (newline)
+
+      (display-list-helper globalList)
+     
+      )
+    
+
   
 
     (define (dispatch method)
@@ -119,9 +242,11 @@
               
               ((eq? method 'add-edge) add-edge)
 
-              ((eq? method 'remove-node) add-edge)
+              ((eq? method 'remove-node) remove-node)
 
-              ((eq? method 'remove-edge) add-edge)
+              ((eq? method 'display) display-list )
+
+              ((eq? method 'remove-edge) remove-edge)
               
 			  (else (lambda() (display "Unknown Request: ")
                                   
@@ -130,4 +255,26 @@
     dispatch)
 
 
-(define (l) (cons 'a '( (b c) ) ) )
+(define G (make-graph))
+((G 'add-node) 'a)       ;=> #t
+((G 'add-node) 'b)       ;=> #t
+((G 'add-node) 'c)       ;=> #t
+((G 'add-node) 'a)       ;=> #f
+	
+((G 'add-edge) 'a 'b)    ;=> #t
+((G 'add-edge) 'a 'c)    ;=> #t
+((G 'add-edge) 'b 'b)    ;=> #t
+((G 'add-edge) 'b 'c)    ;=> #t
+((G 'add-edge) 'c 'd)    ;=> #f
+((G 'display))           ;=> a: b c
+                          
+((G 'remove-edge) 'a 'c) ;=> #t
+((G 'remove-node) 'c)    ;=> #t
+((G 'display))           ;=> a: b
+                            
+
+
+
+
+
+
